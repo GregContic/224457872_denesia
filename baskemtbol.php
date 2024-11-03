991fab32918d87ea553a29f303c10b24328b209c
GregContic <harrydenesia44@gmail.com>
2024-11-03 20:16:47 +0800

    maya kona tapusin inaantok nako HAHAHHAHA

--- baskemtbol.php
+++ baskemtbol.php
@@ -11,7 +11,7 @@
         die("Connection Failed");
     }
     else{
-        echo "Connected Successfully";
+        echo " ";
     }
 
 ?>
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php include('header.php'); ?>
+<?php include('baskemtbol.php'); ?>
+    <div class="box1">
+    <h2>TOP PLAYERS OF THE 2010 DECADE</h2>
+    <button class="btn btn-primary " data-toggle="modal" data-target="#exampleModal">ADD PLAYER</button>
+    </div>
+    <table class="table table-hover table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Ranking</th>
+                <th>Number</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+                <th>Position</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+
+            $query = "select * from `players`";
+            $result = mysqli_query($connection, $query);
+
+            if(!$result){
+                die("Query failed".mysqli_error());
+            }else{
+                while($row = mysqli_fetch_assoc($result)){
+                    ?>
+                        <tr>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['number']; ?></td>
+                            <td><?php echo $row['first_name']; ?></td>
+                            <td><?php echo $row['last_name']; ?></td>
+                            <td><?php echo $row['age']; ?></td>
+                            <td><?php echo $row['position']; ?></td>
+                        </tr>
+
+                    <?php
+                }
+                mysqli_free_result($result);
+            }
+
+            ?>
+            
+            
+        </tbody>
+        
+    </table>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <?php include('footer.php'); ?>
